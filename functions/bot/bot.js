88a4828817f1691271c52b80f2b8d1552d63ee45
Kevin Chan <k3vink3vin@users.noreply.github.com>
2024-07-20 00:50:46 +0100

    Revert fake 400

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -2,7 +2,6 @@ const { Telegraf } = require('telegraf');
 const startAction = require('./actions/start')
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-const eventBody = '';
 
 bot.start(ctx => {
   return startAction(ctx)
@@ -10,14 +9,9 @@ bot.start(ctx => {
 
 exports.handler = async event => {
   try {
-    if (process.env.isLocal) {
-      event.body = eventBody;
-    } else {
-      console.log({event: JSON.stringify(event)});
-    }
-
+    console.log({event: JSON.stringify(event)});
     await bot.handleUpdate(JSON.parse(event.body));
-    return { statusCode: 400, body: '' };
+    return { statusCode: 200, body: '' };
   } catch (e) {
     console.log(e);
     return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
